dd888732f925f1aa4a24ae80a2a237e435e25c5b
NiVerenC <alejancitron100@gmail.com>
2025-05-25 19:12:35 -0300

    Agrego codigo editar producto y eliminar producto

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,43 @@ import SearchBar from './components/SearchBar';
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(''); 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [productToEdit, setProductToEdit] = useState(null);
 
-  const addProduct = (newProduct) => {
-    setProducts([...products, newProduct]);
-    console.log('Lista de productos actual:', [...products, newProduct]); // Para verificar en consola por las dudas
+  const saveProduct = (product) => {
+    if (productToEdit) {
+      setProducts((prevProducts) =>
+        prevProducts.map((p) => (p.id === product.id ? product : p))
+      );
+      setProductToEdit(null);
+    } else {
+      setProducts([...products, product]);
+    }
   };
 
   const handleSearchChange = (term) => {
     setSearchTerm(term);
   };
- const filteredProducts = useMemo(() => {
-    if (!searchTerm) {
-      return products;
+
+  const handleEdit = (product) => {
+    setProductToEdit(product);
+  };
+
+  const cancelEdit = () => {
+    setProductToEdit(null);
+  };
+
+  const handleDelete = (id) => {
+    const confirmDelete = window.confirm('¿Estás seguro de que deseas eliminar este producto?');
+    if (confirmDelete) {
+      setProducts(prevProducts => prevProducts.filter(p => p.id !== id));
     }
+  };
+
+  const filteredProducts = useMemo(() => {
+    if (!searchTerm) return products;
 
     const lowerCaseSearchTerm = searchTerm.toLowerCase();
-
     return products.filter((product) =>
       product.descripcion.toLowerCase().includes(lowerCaseSearchTerm) ||
       product.id.toString().includes(lowerCaseSearchTerm)
@@ -30,21 +50,27 @@ function App() {
   }, [products, searchTerm]);
 
   return (
-     <div className="App">
+    <div className="App">
       <h1>Gestión de Productos</h1>
 
-      <div className="main-content-grid"> 
-
-        <div className="grid-column-left"> 
-          <ProductForm onAddProduct={addProduct} />
+      <div className="main-content-grid">
+        <div className="grid-column-left">
+          <ProductForm
+            onAddProduct={saveProduct}
+            productToEdit={productToEdit}
+            onCancelEdit={cancelEdit}
+          />
         </div>
 
-        <div className="grid-column-right"> 
+        <div className="grid-column-right">
           <SearchBar onSearchChange={handleSearchChange} searchTerm={searchTerm} />
-          <ProductList products={filteredProducts} />
+          <ProductList
+            products={filteredProducts}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
         </div>
-
-      </div> 
+      </div>
     </div>
   );
 }
